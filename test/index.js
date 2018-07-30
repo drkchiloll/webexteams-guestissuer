15f8f984526efebd21a7154dc46f2543c4b5395c
drkchiloll <samwomack_@me.com>
2018-07-30 12:55:09 -0500

    Example code for use with the module

---
+++ test/index.js
@@ -0,0 +1,24 @@
+const { TeamsGuestIssuer } =  require('../dist');
+const { creds: { id, secret }} = require('./config');
+const guest = {
+  userid: '1vfjjgg4bph9',
+  username: 'CE_Emulator'
+};
+
+// AppId + AppSecret (Generated @ developer.webex.com when creating GuestIssuer App)
+const teamsIssuer = TeamsGuestIssuer(id, secret);
+
+const genToken = {
+  userid: guest.userid,
+  user: guest.username
+};
+
+teamsIssuer.generateJwt(genToken)
+  .then(({token}) => {
+    return teamsIssuer.retrieveAuthToken(token)
+      .then(authData => {
+        teamsIssuer.teamsGetUser(authData.token).then(console.log);
+      })
+      .catch(console.log);
+  })
+  .catch(console.log);
