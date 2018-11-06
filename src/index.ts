7ff0bb589315bd8439f372a2ac3a74ef7951646a
drkchiloll <samwomack_@me.com>
2018-11-06 14:37:17 -0600

    rescaled to export a Class only

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,13 @@ export type ReqParams = {
   token: string;
 }
 
-class TeamsApi {
+export class SparkGuest {
+  public appId: string;
+  public appSecret: string;
   private request: AxiosInstance;
-  constructor() {
+  constructor({ id, secret }) {
+    this.appId = id;
+    this.appSecret = secret;
     this.request = axios.create({
       baseURL: 'https://api.ciscospark.com/v1',
       headers: {
@@ -22,96 +26,74 @@ class TeamsApi {
       }
     });
   }
-  getAuthToken(jwt) {
+
+  generateId(cb?: any): any {
+    return uniqid();
+  }
+
+  generateJwt({ userid, displayName }): any {
+    return new Promise((resolve, reject) => {
+      const payload = {
+        sub: userid || uniqid(),
+        name: displayName,
+        iss: this.appId,
+        exp: Math.round(Date.now()/1000) + (90*60) // 6hrs
+      };
+      const decoded = Buffer.from(this.appSecret, 'base64');
+      jwt.sign(payload, decoded, {
+        algorithm: 'HS256',
+        noTimestamp: true
+      }, (e, token) => {
+        if(e) reject(e);
+        else resolve(token);
+      });
+    })
+  }
+
+  getAccessTokenExpiration(expiresIn) {
+    return moment().add(expiresIn, 'seconds').format();
+  }
+  
+  retreiveAccessToken(jwt) {
     return this.request({
       url: '/jwt/login',
       method: 'post',
       headers: { Authorization: `Bearer ${jwt}` }
-    }).then((resp: AxiosResponse) => {
+    }).then((resp: AxiosResponse): any => {
       if(!resp.data || !resp.data.token) {
-        return Promise.reject(
-          'failed to generate an access token: bad response'
-        );
+        return Promise.reject({
+          error: 'failed to generate an access token: bad response'
+        });
       } else {
-        return resp.data;
+        const formattedExpiration =
+          this.getAccessTokenExpiration(resp.data.expiresIn);
+        return {
+          token: resp.data.token,
+          expiresIn: resp.data.expiresIn,
+          formattedExpiration
+        };
       }
-    });
+    })
   }
-  getMe(token) {
+  
+  verifyToken(token) {
+    return jwt.decode(token, { complete: true });
+  }
+
+  getGuest(token) {
     return this.request({
       url: '/people/me',
       method: 'get',
       headers: { Authorization: `Bearer ${token}` }
-    });
+    }).then(resp => resp.data);
   }
-  req({url, method, token, data}) {
+
+  makeRequest({url, token, method, data}) {
     return this.request({
       url,
       method,
-      headers: { Authorization: `Bearer ${token}`},
-      data
-    });
+      data,
+      headers: { Authorization: `Bearer ${token} ` }
+    }).then(resp => resp.data);
   }
 }
-
-const TeamsGuestIssuer = (guestAppId: string, guestAppSecret: string) => {
-  const service: any = {
-    teamsApi: new TeamsApi(),
-    generateId() {
-      return uniqid();
-    },
-
-    generateJwt({userid, user}) {
-      return new Promise((resolve, reject) => {
-        const payload = {
-          sub: userid || this.generateId(),
-          name: user,
-          iss: guestAppId,
-          exp: Math.round(Date.now() / 1000) + (90*60)
-        };
-        const decoded = Buffer.from(guestAppSecret, 'base64');
-        jwt.sign(payload, decoded, {
-          algorithm: 'HS256',
-          noTimestamp: true
-        }, (e: jwt.JsonWebTokenError, token) => {
-          if(e) return reject(e);
-          else {
-            return resolve({success: true, token });
-          }
-        });
-      });
-    },
-
-    tokenExpDate(expires) {
-      return moment().add(expires, 'seconds').format();
-    },
-
-    retrieveAuthToken(token) {
-      return this.teamsApi
-        .getAuthToken(token)
-        .then(authData => {
-          authData['expiration'] =
-            this.tokenExpDate(authData.expiresIn);
-          return authData;
-        })
-        .catch(e => e);
-    },
-
-    verifyToken(token) {
-      return jwt.decode(token, { complete: true });
-    },
-
-    getGuestDetails(token) {
-      return this.teamsApi.getMe(token)
-        .then((resp: any) => resp.data);
-    },
-
-    genericRequest(params: ReqParams) {
-      return this.teamsApi.req(params)
-        .then((resp: any) => resp.data)
-        .catch(err => err);
-    }
-  };
-  return service;
-};
-export { TeamsGuestIssuer };
